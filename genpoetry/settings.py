526c251fe53cb7fbbec44ff855c22bd36fa00563
Ilya Mashintsev <ilya.mashintsev@gcore.com>
2023-04-23 19:09:45 +0400

    Add filters

--- genpoetry/settings.py
+++ genpoetry/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'rest_framework.filters'
 ]
 
 MIDDLEWARE = [
