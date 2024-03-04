d87fa31ed3392a040cebf71f6d16b4674abe628b
Votming <votming@mail.ru>
2024-03-04 18:50:07 +0400

    fix

--- core/filters.py
+++ core/filters.py
@@ -6,9 +6,10 @@ class ArticleFilter(filters.FilterSet):
     shown_times = filters.NumberFilter()
     category = filters.CharFilter(field_name='category__name')
     language = filters.CharFilter(field_name='language__name')
+    status = filters.CharFilter()
 
     ordering = filters.OrderingFilter(fields=(('shown_times', 'shown_times'),))
 
     class Meta:
         model = Article
-        fields = ('id', 'shown_times', 'category', 'language')
+        fields = ('id', 'shown_times', 'category', 'language', 'status')
