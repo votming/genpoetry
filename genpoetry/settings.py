e59f01fa1c1b10800d9e027593626a1899088869
Votming <votming@mail.ru>
2024-03-05 17:29:26 +0400

    go to postgres

--- genpoetry/settings.py
+++ genpoetry/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from configuration import Config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -95,13 +93,8 @@ WSGI_APPLICATION = 'genpoetry.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'generatorson',
-        'USER': 'generatorson',
-        'PASSWORD': Config.DB_PASSWORD,
-        'HOST': Config.DB_IP,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
